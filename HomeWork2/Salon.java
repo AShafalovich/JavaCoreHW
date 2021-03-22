fb5dd62997133658d1073bdb856d5331f2b8260f
AShafalovich <ant1337@mail.ru>
2021-03-23 02:26:33 +0300

    hw 2

---
+++ HomeWork2/Salon.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Salon {
+
+    private ArrayList<Object> order = new ArrayList<Object>();
+
+    public void setOrder(Object... objects){
+
+        order.clear();
+        for (int i = 0; i < objects.length; i++) {
+
+            order.add(i, objects[i]);
+        }
+    }
+
+    public ArrayList<Object> getOrder() {
+
+        return order;
+    }
+}
